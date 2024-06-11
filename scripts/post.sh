817430c2838f8d67c5b7aa42a6df3aab747ee331
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-11 20:38:18 +0200

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -4,3 +4,7 @@
 # Use this file to execute commands after configuration finishes
 ################################################################
 
+
+
+# Add tiling to i3 config if i3 version is 4.21 or higher
+echo "i3 version: `i3 --version`"
