f9f12ccb521a26dcaac39058643c63fa77efd468
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-11 20:54:35 +0200

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -7,6 +7,7 @@
 
 
 # Add tiling to i3 config if i3 version is 4.21 or higher
-echo "i3 version: `i3 --version`"
 i3_version=$(i3 --version | grep -oP "(?<=\bversion\s)\S+")
+echo "i3 version: $i3_version"
+
 
