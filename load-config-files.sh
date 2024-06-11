fd99076a4552dd45b91180cf0ba5bad17cbac673
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-11 17:40:04 +0200

    Update load-config-files.sh

--- load-config-files.sh
+++ load-config-files.sh
@@ -59,7 +59,7 @@ do
      		fi
      		if [ $index == 1 ]; then
 			mkdir -p `dirname "${to/#~\//$HOME\/}"` 
-   			echo $to
+   			echo "$to   $path"
 			if [[ $to == */ ]]
 			then
    				if [[ -d $config_directory/$path/`basename $to` ]]
