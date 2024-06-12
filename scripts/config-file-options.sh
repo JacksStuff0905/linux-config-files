fb97d31367638ba442135c235d2710d2e5b75487
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-12 13:19:28 +0200

    Update config-file-options.sh

--- scripts/config-file-options.sh
+++ scripts/config-file-options.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 # This script enables options which are conditional in config files
+# to enable syntax add an empty line with <use-options> somwhere in your file
+# options syntax: <some-command> will be replaced with a # if the command fails and will be removed if the command succedes
 
+script_dir="$(dirname "$(realpath "$0")")"
+
+config_folder="$script_dir/../config-files/"
+
+
+function process_file {
+  echo $1
+}
+
+
+find $config_folder -type f -exec  {} \;
