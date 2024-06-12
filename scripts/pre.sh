7b7e09575f45a0a3b82c276fee686a8ca713b0b1
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-12 13:20:11 +0200

    Update pre.sh

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -3,3 +3,16 @@
 ###############################################################
 # Use this file to execute commands before configuration begins
 ###############################################################
+
+
+# Install all needed snap packages
+while [[ $install_snap_pckg != y ]] && [[ $install_snap_pckg != n ]]
+do
+	printf "\e[93mInstall snap packages? (These packages are optional and might impact performance on slow systems) [y/n] \e[0m"
+	read install_snap_pckg
+done
+if [[ install_snap_pckg == y ]]
+then
+	bash $script_dir/scripts/snap-install-remove.sh
+fi
+echo ""
