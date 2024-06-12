e700aab29b1d78a81bb23dcd2fe1553f4399a315
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-12 12:11:23 +0200

    Update snap-install-remove.sh

--- scripts/snap-install-remove.sh
+++ scripts/snap-install-remove.sh
@@ -10,7 +10,7 @@ removes_file="$script_dir/../actions/snap-remove"
 
 
 # Install packages
-printf "\e[93mInstalling snap packages...\e[0m\n"
+printf "\t\e[93mInstalling snap packages...\e[0m\n"
 
 while IFS= read -r line
 do
@@ -25,11 +25,11 @@ do
  	grep_result=$(snap list | grep -oP "^$trimmed(?=\s+\d)")
  	if [ $grep_result == $trimmed ]
    	then
-    		printf "\e[32mSnap package $trimmed already installed; skipping\e[0m\n"
+    		printf "\t\t\e[32mSnap package $trimmed already installed; skipping\e[0m\n"
       		continue
      	else
       		{
-	 		printf "\e[93mInstalling/updating snap package $trimmed...\e[0m "
+	 		printf "\t\t\e[93mInstalling/updating snap package $trimmed...\e[0m "
 			install_output=$(sudo snap install $trimmed)
 			printf "\e[32mInstall/update was succesfull\e[0m\n"
   		
@@ -37,11 +37,11 @@ do
   	fi
 	
 done < $installs_file
-printf "\e[32mFinished installing snap packages\e[0m\n"
+printf "\t\e[32mFinished installing snap packages\e[0m\n"
 
 
 # Remove packages
-printf "\e[93mRemoving snap packages...\e[0m\n"
+printf "\t\e[93mRemoving snap packages...\e[0m\n"
 
 while IFS= read -r line
 do
@@ -56,13 +56,13 @@ do
 	if snap list | grep -oP "^$trimmed(?=\s+\d)"
    	then
     		{
-	  		printf "\e[93mRemoving snap package $trimmed...\e[0m "
+	  		printf "\t\t\e[93mRemoving snap package $trimmed...\e[0m "
 			remove_output=$(sudo snap remove $trimmed)
 			printf "\e[32mRemoving was succesfull\e[0m\n"
 		} || printf "\e[31mRemoving failed - Printing output: $remove_output\e[0m\n" 
      	else
-      		printf "\e[32mSnap package $trimmed isn't installed; skipping\e[0m\n"
+      		printf "\t\t\e[32mSnap package $trimmed isn't installed; skipping\e[0m\n"
   	fi
 	
 done < $removes_file
-printf "\e[32mFinished removing snap packages\e[0m\n"
+printf "\t\e[32mFinished removing snap packages\e[0m\n"
