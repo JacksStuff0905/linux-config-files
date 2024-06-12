35e6afb93d7f6b7b302443260aa2004e87e421cb
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-12 14:53:35 +0200

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -10,8 +10,8 @@ script_dir="$(dirname "$(realpath "$0")")"
 # Add tiling to i3 config if i3 version is 4.21 or higher
 i3_config_file="$HOME/.config/i3/config"
 
-i3_version=$(i3 --version | grep -oP "(?<=\bversion\s)\S+")
-if [[ $(printf "%s\n" "$i3_version" "4.21" | sort -V | head -n 1) != "$i3_version" ]]
-then
-    sed -i 's/#\s*\b\(tiling_drag\)\b/\1/' $i3_config_file
-fi
+#i3_version=$(i3 --version | grep -oP "(?<=\bversion\s)\S+")
+#if [[ $(printf "%s\n" "$i3_version" "4.21" | sort -V | head -n 1) != "$i3_version" ]]
+#then
+#    sed -i 's/#\s*\b\(tiling_drag\)\b/\1/' $i3_config_file
+#fi
