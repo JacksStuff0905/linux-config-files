478b7337c1be12ad3123c27dbc049edd383bcafe
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-06-13 15:51:49 +0200

    Update flatpak.sh

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -20,11 +20,24 @@ do
 	then
 		continue
 	fi
-	
- 	
+ 
+ 	words=($trimmed)
+    	length="${#words[@]}"
+
+      	
+  	link=false
+ 	if [[ "$line" == "&link install"* ]]
+ 	then
+		link=true
+  		trimmed="${trimmed#&link install}"
+    	fi
   	{
 	 	printf "\t\t\e[93mPerforming flatpak command $trimmed...\e[0m "
 		operation_output=$(sudo flatpak -y $trimmed &> /dev/null)
+  		if [[ $link == true ]]
+    		then
+      			sudo ln -s "/var/lib/flatpak/exports/bin/${words[$length - 1]}" "/usr/bin/${words[1]}"
+      		fi
 		printf "\e[32mFlatpak command was succesfull\e[0m\n"
   		
 	} || printf "\e[31mFlatpak command failed - Printing output: $operation_output\e[0m\n"
